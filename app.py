021b1e6b0190d6c40ed5d4f30ab9774afdadfb57
Huang <mhuang@ediscovery.com.tw>
2021-03-11 22:28:49 +0800

    add files

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('Your Channel Access Token')
+#line_bot_api = LineBotApi('Your Channel Access Token')
+line_bot_api = LineBotApi('p/HeZOW7Km+JlKE/usfDH/vXRgbgwM9t66nI+EyaVONq0yRLAtLuPkJq0rVD5cgs2vB3Unmh0RLh9g5gw6/LmZx/Q61DtYXk0X3j6ES4zKXoRQjyj+vBsRTmv+Ntbw+Nw7PT2b0q38J/rae8M+BLswdB04t89/1O/w1cDnyilFU=')
+
 # Channel Secret
-handler = WebhookHandler('Your Channel Secret')
+#handler = WebhookHandler('Your Channel Secret')
+handler = WebhookHandler('c17bfd722b7e54a50df6f6bee3a9f5fb')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -36,51 +39,51 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     Content=event.message.text #輸入內容
-    if Content == '0050':
-        url = 'https://goodInfo.tw/StockInfo/StockDividendPolicy.asp?STOCK_ID=0050' #0050網址
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
-        re=requests.post(url, headers = headers)
-        #soup = BeautifulSoup(re.content, "lxml") 
-        soup = BeautifulSoup(re.content, "html.parser")  
-        data=soup.find_all("table", class_="solid_1_padding_3_1_tbl")
-        for i in data:
-            Info=list(i.stripped_strings) #去除多餘空白
-            #print(Info) #股票內容
-            #print('股票名稱:'+Info[0])
-            #print('日期:'+Info[3])
-            #print('開盤:'+Info[17]) 
-            #print('收盤:'+Info[12]) 
-            #print('最高:'+Info[18]) 
-            #print('最低:'+Info[19]) 
-            #print('漲跌幅:'+Info[15])
 
-        Stock_Name=str(Info[0]) #名稱
-        Stock_Date=str(Info[3]) #日期
-        Stock_Open=str(Info[17]) #開盤
-        Stock_Close=str(Info[12]) #收盤
-        Stock_High=str(Info[18]) #最高
-        Stock_Low=str(Info[19]) #最低
-        Stock_Quote_Change=str(Info[15]) #漲跌幅
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='股票名稱:{}\n{}\n開盤:{}\n收盤:{}\n最高:{}\n最低:{}\n漲跌幅:{}\n'.format(Stock_Name,Stock_Date,Stock_Open,Stock_Close,Stock_High,Stock_Low,Stock_Quote_Change))) #回復訊息
+    if Content == '股票':
+        line_bot_api.reply_message(
+            event.reply_token, TemplateSendMessage(
+            alt_text = 'Buttons template',
+            template = ButtonsTemplate(
+                title='常用股票',
+                text='請選擇股票代號',
+                actions=[
+                    MessageTemplateAction(
+                         label='0050',
+                         text='0050'
+                    ),
+                    MessageTemplateAction(
+                         label='0056',
+                         text='0056'
+                    ),
+                    MessageTemplateAction(
+                         label='2330',
+                         text='2330'
+                    )
+                ]
+            )
+        ) 
+    )
+    else:
+        try:
+            url = 'https://goodInfo.tw/StockInfo/StockDividendPolicy.asp?STOCK_ID={}'.format(event.message.text) #Stock網址
+            headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+            re=requests.post(url, headers = headers)
+            soup = BeautifulSoup(re.content, "html.parser")  
+            data=soup.find_all("table", class_="solid_1_padding_3_1_tbl")
+            for i in data:
+                Info=list(i.stripped_strings) #去除多餘空白
 
-    elif Content == '0056':
-        url = 'https://goodInfo.tw/StockInfo/StockDividendPolicy.asp?STOCK_ID=0056' #0056網址
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
-        re=requests.post(url, headers = headers)
-        #soup = BeautifulSoup(re.content, "lxml") 
-        soup = BeautifulSoup(re.content, "html.parser")  
-        data=soup.find_all("table", class_="solid_1_padding_3_1_tbl")
-        for i in data:
-            Info=list(i.stripped_strings) #去除多餘空白
-
-        Stock_Name=str(Info[0]) #名稱
-        Stock_Date=str(Info[2]) #日期
-        Stock_Open=str(Info[16]) #開盤
-        Stock_Close=str(Info[11]) #收盤
-        Stock_High=str(Info[17]) #最高
-        Stock_Low=str(Info[18]) #最低
-        Stock_Quote_Change=str(Info[14]) #漲跌幅
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='股票名稱:{}\n{}\n開盤:{}\n收盤:{}\n最高:{}\n最低:{}\n漲跌幅:{}\n'.format(Stock_Name,Stock_Date,Stock_Open,Stock_Close,Stock_High,Stock_Low,Stock_Quote_Change))) #回復訊息
+            Stock_Name=str(Info[0]) #名稱
+            Stock_Date=str(Info[3]) #日期
+            Stock_Open=str(Info[17]) #開盤
+            Stock_Close=str(Info[12]) #收盤
+            Stock_High=str(Info[18]) #最高
+            Stock_Low=str(Info[19]) #最低
+            Stock_Quote_Change=str(Info[15]) #漲跌幅
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='股票名稱:{}\n{}\n開盤:{}\n收盤:{}\n最高:{}\n最低:{}\n漲跌幅:{}\n'.format(Stock_Name,Stock_Date,Stock_Open,Stock_Close,Stock_High,Stock_Low,Stock_Quote_Change))) #回復訊息
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='股票代號輸入錯誤,請重新輸入!')) #回復訊息
     
 
 import os
